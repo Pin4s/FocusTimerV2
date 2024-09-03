4544998a2731b4738fb55a163b82905b4764e2bd
Pedro Pinas <135555435+Pin4s@users.noreply.github.com>
2024-09-03 17:03:53 -0300

    Delete FocusTimer - Versão 2.0 directory

--- FocusTimer - Versão 2.0/src/FocusTimer/elements.js
+++
@@ -1,9 +0,0 @@
-
-export const controls = document.getElementById('timer-controls')
-
-export const soundControls = document.getElementById('toggle-theme')
-
-export const minutes = document.getElementById('minutes')
-
-export const seconds = document.getElementById('seconds')
-
